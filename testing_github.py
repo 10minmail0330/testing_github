a60e9a94c336fe05bfb8ba260cafa4eb3adf562e
Hon Foong <48639627+foong8@users.noreply.github.com>
2019-12-02 15:50:53 +0800

    Update testing_github.py

--- testing_github.py
+++ testing_github.py
@@ -5,6 +5,9 @@ def main():
     print("Hello World, GitHUb Here")
     print("Ediedted line 2 ")
     print("This is testing branch")
+    
+    print("second time edit branch")
+    
 if __name__ == "__main__":
     main()
 
